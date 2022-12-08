020b9d3e90f81c308a2dd78c50c101794542a970
Liam <liamk15@gmail.com>
2022-12-08 14:04:31 +0000

    Tweak install script

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-if [ ! -d ./bundle/Vundle.vim ]; then
-    echo "Installing Vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
-else
-    echo "Vundle Already Installed"
-fi
 if [[ -L ~/.vimrc ]]; then
     echo "Already Symlinked"
 else
@@ -12,4 +6,4 @@ else
     ln -s ~/.vim/.vimrc ~/.vimrc
 fi
 echo "Installing Plugins"
-vim +PluginInstall +qall
+vim +PlugInstall +qall
